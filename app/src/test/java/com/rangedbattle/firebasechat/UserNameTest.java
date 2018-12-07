988ff213eadbfca477509950df62ab5a0620b9b4
WBales <34966325+WBales@users.noreply.github.com>
2018-12-07 16:42:21 -0600

    Added javadoc for EmailTest, PasswordTest, and UserNameTest

--- app/src/test/java/com/rangedbattle/firebasechat/UserNameTest.java
+++ app/src/test/java/com/rangedbattle/firebasechat/UserNameTest.java
@@ -5,6 +5,9 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+/**This class tests the method that tests the user names
+ *
+ */
 public class UserNameTest {
 
     private RegisterActivity tester;
